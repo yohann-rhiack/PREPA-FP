8ccd1bda0b9aab1651986e5d8d0b0988ff66236e
yohann-rhiack <yohannrhiack@gmail.com>
2025-03-17 15:56:10 +0000

    Création et migration des tables dans la bd

---
+++ database/migrations/2025_03_17_153612_create_attempts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attempts', function (Blueprint $table) {
+            $table->id();
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->foreignId('test_id')->constrained('tests')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attempts');
+    }
+};
